64365e057ffa4772df6c009ff5edb0c95c025070
Rohit chawla <60376165+Rohitchawla7042@users.noreply.github.com>
2022-04-20 22:11:55 +0530

    Windmil.css (55th Challenge)

---
+++ CSS Battle 11/Windmill.cs
@@ -0,0 +1,40 @@
+<span></span><span></span><span></span><span></span><style>body {
+    background: #191919;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+span {
+    width: 50px;
+    height: 100px;
+    background: #F9E492;
+    border-radius: 100px 0px 0px 100px;
+    transform: rotate(90deg);
+    position: absolute;
+    top: 75px;
+    left: 125px;
+}
+
+span:nth-of-type(2) {
+    transform: rotate(180deg);
+    left: 199px;
+    top: 50px;
+    background: #4F77FF;
+}
+
+span:nth-of-type(3) {
+    transform: rotate(270deg);
+    left: 225px;
+    top: 125px;
+    background: #F9E492;
+}
+
+span:nth-of-type(4) {
+    transform: rotate(360deg);
+    left: 150px;
+    top: 150px;
+    background: #4F77FF;
+}
+
+</style>
